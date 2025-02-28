e8d128a75aaca1de3986cb7eaaf0f17700bd278d
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2025-02-28 20:28:34 +0530

    Day 12 - React Basics, React App'

---
+++ Day12/day12-react-app/app.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+
+const domRoot = document.getElementById("parent");
+const reactRoot = ReactDOM.createRoot(domRoot);
+
+const Title = <h1>Hello</h1>;
+
+reactRoot.render(Title);
