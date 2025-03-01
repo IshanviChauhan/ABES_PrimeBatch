dcac5306ccf6b9c4d7ec13351b4e2f14988be30c
Ishanvi Chauhan <ishanvi.chauhan@gmail.com>
2025-03-02 00:22:26 +0530

    Youtube clone header and sidenav completed

---
+++ Practice/Yt_Clone/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.display= "flex";
+    console.log("Side Nav Opened");
+}
+
+function closeNav() {
+    document.getElementById("sidenav").style.display = "none";
+    console.log("Side Nav Closed");
+}
